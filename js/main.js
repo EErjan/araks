c9b607cde407bdbc8546d0a3aadd2c2e595f6a1e
Erjan Ergashev <erjan.ergashev@gmail.com>
2022-09-21 12:17:58 +0500

    update home page

--- js/main.js
+++ js/main.js
@@ -32,3 +32,44 @@ try {
 }
 
 
+// Mobile accordion
+
+
+try {
+    $(function () {
+        var Accordion = function (el, multiple) {
+            this.el = el || {};
+            this.multiple = multiple || false;
+
+            // Variables
+            var link = this.el.find('.link');
+            // Eventos
+            link.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+        }
+
+        Accordion.prototype.dropdown = function (e) {
+            var $el = e.data.el;
+            $this = $(this),
+                $next = $this.next();
+
+            // Desencadena evento de apertura en los elementos siguientes a la clase link = ul.submenu
+            $next.slideToggle();
+            // Agregar clase open a elemento padre del elemento con clase link = li
+            $this.parent().toggleClass('open');
+
+            //Parametro inicial que permite ver 1 solo submenu abierto
+            if (!e.data.multiple) {
+                $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+                $el.find('.submenu2').not($next).slideUp().parent().removeClass('open');
+                $el.find('.accordion__content').not($next).slideUp().parent().removeClass('open');
+                $el.find('.accordion__content2').not($next).slideUp().parent().removeClass('open');
+            }
+
+        }
+        // Elegir submenus multiples (true) submenus uno a la vez (false)
+        var accordion = new Accordion($('#accordion'), false);
+        var accordion = new Accordion($('#accordion-2'), false);
+    });
+} catch (e) {
+
+}
